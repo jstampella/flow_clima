023b8b53f0ed472eb049686f70a91e923a0c024b
jonathan <jona.stampella@gmail.com>
2022-07-10 23:17:07 -0300

    fix http to https

--- src/app/api/weatherApi.js
+++ src/app/api/weatherApi.js
@@ -25,9 +25,9 @@ export async function getWeatherCurrentApi(cityValue, cord) {
 export async function getWeatherApi(cityValue, cord) {
 	let API_URL = null;
 	if (cityValue)
-		API_URL = `http://api.openweathermap.org/data/2.5/forecast?q=${cityValue}&units=metric&appid=${WEATHER_KEY}&lang=es`;
+		API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityValue}&units=metric&appid=${WEATHER_KEY}&lang=es`;
 	else
-		API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${cord.latitude}&lon=${cord.longitude}&units=metric&appid=${WEATHER_KEY}&lang=es`;
+		API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${cord.latitude}&lon=${cord.longitude}&units=metric&appid=${WEATHER_KEY}&lang=es`;
 	if (API_URL)
 		return axios
 			.get(API_URL)
